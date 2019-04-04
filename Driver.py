e5b445714a017f06c4894c1154b9b871b98a7196
Lambrecht <v21m972@msu.montana.edu>
2019-04-04 16:27:13 -0600

    tried moving camera stuff outside the functions

--- Driver.py
+++ Driver.py
@@ -13,17 +13,17 @@ face_search = SearchForFace(client)
 distance_tolerance = 0.1  # don't move to the face if within this distance of 1 unit
 
 # global variables
-no_face_search_restart_interval = 0  # min time interval required to restart face search
+no_face_search_restart_interval = 3  # min time interval required to restart face search
 last_face_time = -no_face_search_restart_interval  # last time a face was seen
 
 
-def move_to_face():
+def move_to_face(image):
     """
     moving to offset away from face state
     :param distance: the distance to the face
     :return: False if face is lost
     """
-    face = face_search.search_for_face()
+    face = face_search.search_for_face(image)
     face_search.move_forward_or_back(face)
 
     return False
@@ -47,40 +47,31 @@ def searching(image):
         return False, 0
 
 
-def tracking_face():
+def tracking_face(image):
     """
     tracking face state
     :return: False if face is lost
     """
-    face = face_search.search_for_face()
+    face = face_search.search_for_face(image)
     face_search.track_face(face)
     return False
 
 
-def rotate_to_face():
+def rotate_to_face(image):
     """
     rotating to center on face state
     :return: False if face is lost
     """
-    face = face_search.search_for_face()
+    face = face_search.search_for_face(image)
     face_search.center_on_face(face)
     return False
 
 
-def running_loop():
+def running_loop(image):
     search_state = True
     rotate_state = False
     moving_state = False
     tracking_state = False
-    dis = 0
-    camera = PiCamera()
-    w, h = 320, 240
-    camera.resolution = (w,h)
-    camera.framerate=32
-    rawCapture = PiRGBArray(camera, size=camera.resolution)
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
-        rawCapture.truncate(0)
         while True:
             if search_state:
                 # search for a face, once found go on to rotate state unless still in tracking state
@@ -90,25 +81,29 @@ def running_loop():
                     rotate_state = not tracking_state
 
             elif rotate_state:
-                face_found = rotate_to_face()
+                face_found = rotate_to_face(image)
                 rotate_state = False
                 if not face_found:
                     search_state = True
 
             elif moving_state:
-                face_found = move_to_face()
+                face_found = move_to_face(image)
                 moving_state = False
                 if not face_found:
                     search_state = True
 
             elif tracking_state:
                 # only exit tracking state to do some searching then go right back to tracking
-                face_found = tracking_face()
+                face_found = tracking_face(image)
                 timeout = time.process_time() - last_face_time < no_face_search_restart_interval
                 if not face_found:
                     if not timeout:
                         search_state = True
-
-
-
-running_loop()
+camera = PiCamera()
+w, h = 320, 240
+camera.resolution = (w,h)
+camera.framerate=32
+rawCapture = PiRGBArray(camera, size=camera.resolution)
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+    running_loop(image)
