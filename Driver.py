98adde95fb6553db605f21b4855f97bba774bf48
John Lambrecht <jclambrecht97@msn.com>
2019-04-05 15:00:42 -0600

    added search for face

--- Driver.py
+++ Driver.py
@@ -92,8 +92,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     cv.imshow("image", image)
 
-    search_for_face(image, face)
-
+    #search_for_face(image, face)
+    tango.setTarget(HEADTURN, 7900)
+    tango.setTarget(HEADTILT, 7900)
     key = cv.waitKey(1) & 0xFF
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
