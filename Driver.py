2767a3695a0bc47c340ce636ac1dcebffc11285d
John Lambrecht <jclambrecht97@msn.com>
2019-04-05 14:58:50 -0600

    added search for face

--- Driver.py
+++ Driver.py
@@ -63,11 +63,12 @@ def move_head(turn, value):
 
 def search_for_face(image, face):
     w, h = image.shape[:2]
-
+    tango.setTarget(HEADTILT, 3000)
     for y in range(h):
         move_head(False, 200)
-        for y in range(h):
-            move_head(True, 200)
+        tango.setTarget(HEADTURN, 7900)
+        for x in range(w):
+            move_head(True, -200)
             if face is not None:
                 pass
         if face is not None:
