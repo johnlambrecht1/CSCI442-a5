436040333bf9ca7e8ca129608e849ac8b749a2aa
John Lambrecht <jclambrecht97@msn.com>
2019-04-05 14:22:15 -0600

    baby step

--- Driver.py
+++ Driver.py
@@ -1,22 +1,33 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-import cv2 as cv
 import time
+import cv2 as cv
 
-
-
-
-camera = PiCamera()  # why doesn't it like this line?
-
-w, h = 640, 480
-camera.resolution = (w,h)
-camera.framerate=32
+camera = PiCamera()
+w, h = 320, 240
+camera.resolution = (w, h)  # (640, 480)
+camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=camera.resolution)
-print("test2")
-time.sleep(.1)
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+# capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
     image = frame.array
-    print("test1")
-    #running_loop(image, rawCapture)
+    # get frame size
+    w, h = image.shape[:2]
+    # use the lower center of the image
+    #x, y = w//5, h//2
+    #image = image[y:y + h//4, x:x + 3*w//5]
     cv.imshow("image", image)
-    rawCapture.truncate()
+    # do one loop
+    #path_follow.pi_cam_loop(image)
+    # move
+    #path_follow.perform_movement()
+
+    key = cv.waitKey(1) & 0xFF
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
