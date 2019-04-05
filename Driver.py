e58b38ad13624f98ccb9af4258e99099f5b6a9ce
John Lambrecht <jclambrecht97@msn.com>
2019-04-05 14:27:47 -0600

    baby step

--- Driver.py
+++ Driver.py
@@ -11,7 +11,7 @@ rawCapture = PiRGBArray(camera, size=camera.resolution)
 
 # allow the camera to warmup
 time.sleep(0.1)
-
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
@@ -23,10 +23,11 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     #x, y = w//5, h//2
     #image = image[y:y + h//4, x:x + 3*w//5]
     cv.imshow("image", image)
-    # do one loop
-    #path_follow.pi_cam_loop(image)
-    # move
-    #path_follow.perform_movement()
+
+    face = face_cascade.detectMultiScale(image, 1.8, 5)
+
+    for (x,y,w,h) in face:
+        cv.rectangle(image, (y,x), (y+h, x+w), (0,0,255), 3)
 
     key = cv.waitKey(1) & 0xFF
     # clear the stream in preparation for the next frame
