a12799561ee620d46a5bd92f5d059712e7fb8547
John Lambrecht <jclambrecht97@msn.com>
2019-04-05 14:54:20 -0600

    added search for face

--- Driver.py
+++ Driver.py
@@ -64,9 +64,9 @@ def move_head(turn, value):
 def search_for_face(image, face):
     w, h = image.shape[:2]
     for x in range(w):
-        move_head(True, x)
+        move_head(True, 200)
         for y in range(h):
-            move_head(False, y)
+            move_head(False, 200)
             if face is not None:
                 pass
         if face is not None:
